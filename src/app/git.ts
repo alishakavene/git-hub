56fda6c41572ea8f142eddc285e17c4c38ccb880
alishakavene <akmunywoki@gmail.com>
2020-07-01 11:56:28 +0300

    add git .ts file

---
+++ src/app/git.ts
@@ -0,0 +1,18 @@
+export class Git {
+  {
+    name: 'Niklaus Mikaelson',
+    description: '"Mercy is for the Weak."',
+    id:'peanutbutter_nova.'
+
+  },
+  {
+    name: 'Elijah Mikaelson',
+    description: '"I believe that when you love someone and that someone returns the love you are uniquely vulnrable. They have a power to hurt you like nothing else."',
+    id: 'barbie'
+  },
+  {
+    name: 'Marcellus Gerard',
+    description: '"Let us make one thing clear you will never have this -Loyalty. You can not buy it, You can not own it, You can not force it, it comes only out of love and the respect for the people you believe in."',
+    id:'Grishon'
+  },
+}
