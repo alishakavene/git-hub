972f3f44aa49c7143a1fb1cfa276447d9f905bae
alishakavene <akmunywoki@gmail.com>
2020-07-01 11:58:22 +0300

    add file gitcard

---
+++ src/app/gitcard/gitcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gitcard',
+  templateUrl: './gitcard.component.html',
+  styleUrls: ['./gitcard.component.css']
+})
+export class GitcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
