670861d5763bb337bd1b112a9765c78fb514db65
alishakavene <akmunywoki@gmail.com>
2020-07-01 12:01:05 +0300

    add github

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {  GithubService } from '../github-service/github.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+   github:any;
+  constructor(private githubService:GithubService) {
+    this.githubService.getGitHub().subscribe(github =>{
+       console.log(github);
+       this.github = github;
+    }
+    )
+
+  }
+//searchUser(){this.GitHubservice.findUser(this.username)}
+  ngOnInit(): void {
+  }
+
+}
